20606083a71e29088e2ffbf6f08b053d60e988d5
Ayose <ayosec@gmail.com>
2026-04-24 00:00:00 +0000

    tests: simple runner
    Very minimalistic runner, that executes all tests/**/test_*.lua files in
    headless Neovim processes.
    
    Plenary is deprecated, so it should not be used. If this runner is too
    simple, it can be replaced later with mini.test.

---
+++ tests/asserts.lua
@@ -0,0 +1,34 @@
+local debug = require("debug")
+
+local M = {}
+
+local function error(message)
+    local info = debug.getinfo(3)
+    vim.print(string.format(
+        "[%s:%s] %s\n",
+        info.short_src,
+        info.currentline,
+        message
+    ))
+    vim.cmd "1cq"
+end
+
+M.eq = function(a, b)
+    if not vim.deep_equal(a, b) then
+        error(string.format(
+            "%s not equal to %s",
+            vim.inspect(a),
+            vim.inspect(b)
+        ))
+    end
+end
+
+setmetatable(M, {
+    __call = function(_, result, message)
+        if not result then
+            error(message or "assertion failed")
+        end
+    end,
+})
+
+return M
